2736a510bab8964a55853a9de9693bf66a19fa41
centraldoblogger <56167143+centraldoblogger@users.noreply.github.com>
2020-08-06 21:53:42 -0300

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var settings = {
     'radio_name': 'Brasil Hits',
 
     // URL of streaming without / on the final, eg: http://streaming.com:8080
-    'url_streaming': 'http://radio2.suaradionanet.net:10839/stream',
+    'url_streaming': 'http://radio2.suaradionanet.net:10839',
 
     // When choosing icecast, make sure the file 'player.log' in the document root is writeable.
     // Choose between 'shoutcast' or 'icecast'
